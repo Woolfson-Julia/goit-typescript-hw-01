72912371d51f494e98c1773251e63c32b36992d3
Woolf <woolfson1995@gmail.com>
2025-04-27 17:41:41 +0300

    fix 2.ts generics

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -1,11 +1,11 @@
 type AllType = {
-  name?: string;
-  position?: number;
-  color?: string;
-  weight?: number
+  name: string;
+  position: number;
+  color: string;
+  weight: number
 }
 
-function compare (top:AllType, bottom:AllType):Pick<AllType, 'name' | 'color' | 'position' | 'weight'> {
+function compare (top:Pick<AllType, 'name' | 'color'>, bottom:Pick<AllType, 'position' | 'weight'>):AllType {
   return {
     name: top.name,
     color: top.color,
